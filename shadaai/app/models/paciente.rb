50a6576c4cab4e792706d6fd67c84b648e0394b3
aldveq <aldveq80@gmail.com>
2016-01-11 15:45:15 -0600

    Delete paciente.rb

--- shadaai/app/models/paciente.rb
+++
@@ -1,9 +0,0 @@
-class Paciente < ActiveRecord::Base
-	validates :primerN, presence: true
-	validates :segundoN, presence: true
-	validates :primerA, presence: true
-	validates :segundoA, presence: true
-	validates :edad, presence: true, numericality: true
-	validates :direccion, presence: true
-	validates :telefono, presence: true, numericality: true, uniqueness: true					
-end
